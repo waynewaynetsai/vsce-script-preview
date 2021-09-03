bafe9937128bd53c90dcc36692f119b331fbd31c
waynewaynetsai <wayne040904007@gmail.com>
2021-09-03 16:33:32 +0800

    style(library.d.ts): remove empty string

--- src/typings/library.d.ts
+++ src/typings/library.d.ts
@@ -48,7 +48,7 @@ export interface LibraryApi {
   };
   fs: {
     getCurrentWorkspaceFolder: () => vscode.WorkspaceFolder | undefined;
-    copyFileOrFolder: (source: string, target: string, option: { overwrite: boolean }) => Thenable<void>;
+    copyFileOrFolder: (source: string, target: string, option?: { overwrite: boolean }) => Thenable<void>;
     createNewFile: (filename: string, content: string, fsPath?: string) => Thenable<void>;
     createNewFolder: (name: string, fsPath?: string) => Thenable<void>; 
   },
