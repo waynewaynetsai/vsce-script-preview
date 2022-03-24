d20c9ec0d73ba596f19a89a4c6a5313f8f0c8c9f
waynewaynetsai <wayne040904007@gmail.com>
2022-03-24 17:05:19 +0800

    feat(ScriptLoader): implement script loader

---
+++ src/loader.ts
@@ -0,0 +1,71 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { ExtensionContext } from 'vscode';
+import { WorkspaceConfig } from './config';
+
+const vscodeApi: typeof vscode = require('vscode');
+
+declare const __webpack_require__: any;
+declare const __non_webpack_require__: any;
+
+interface VsceScriptModule {
+    vscode: typeof vscode;
+    activate(context: ExtensionContext, ...args: any[]): void;
+    deactivate(): void;
+}
+
+export class ScriptLoader implements vscode.Disposable {
+
+    public cacheScript!: VsceScriptModule;
+
+    private static _instance: ScriptLoader;
+
+    static instantiate(context: vscode.ExtensionContext, lib) {
+       const instance = new ScriptLoader(context);
+       instance.injectGlobalDependencies(lib); 
+       instance.load(context);
+       context.subscriptions.push(instance);
+       ScriptLoader._instance = instance; 
+       return instance;
+    }
+
+    static getInstance(context: vscode.ExtensionContext, lib) {
+       if (!ScriptLoader._instance) {
+           ScriptLoader.instantiate(context, lib); 
+       }
+       return this._instance = ScriptLoader._instance; 
+    }
+
+    constructor(private context: ExtensionContext) {} 
+    
+    public injectGlobalDependencies(lib) {
+        global.vscode = vscodeApi;
+        global.lib = lib;
+    }
+
+    private require(scriptPath: string) {
+        const requireModule = typeof __webpack_require__ === "function" ? __non_webpack_require__ : require;
+        delete requireModule.cache[requireModule.resolve(scriptPath)];
+        this.cacheScript = requireModule(scriptPath);
+        return this.cacheScript;
+    }
+
+    public load(_context: ExtensionContext) {
+        try {
+            const configPath = vscode.workspace.getConfiguration().get<string>(WorkspaceConfig.ProjectPath)!;
+            if (configPath === '') {
+                vscode.window.showErrorMessage(`Should given script_path for vsce-script!`);
+                return;
+            }
+            const scriptPath = path.normalize(path.relative(__dirname, configPath));
+            const script = this.require(scriptPath);
+            script.activate(this.context);
+        } catch (error) {
+            console.error('ScriptLoader:error', error);
+        }
+    }
+
+    public dispose() {
+        this.cacheScript?.deactivate();
+    }
+}
