59ed49eb12d55d56349ffe873a13e7f5216382a5
waynewaynetsai <wayne040904007@gmail.com>
2022-03-24 19:22:26 +0800

    feat(command.ts): implement completionRegisterFactory

--- src/command/command.ts
+++ src/command/command.ts
@@ -1,35 +1,43 @@
-import * as vscode from 'vscode';
+import * as vsc from 'vscode';
 import * as cp from 'child_process';
 import { logger } from '../logger';
 
-export function commandRegisterFactory(context: vscode.ExtensionContext) {
+export function commandRegisterFactory(context: vsc.ExtensionContext) {
 	const registerCommand = (commandId: string, commandHandler: (...args: any[]) => any) => {
-		const subscription = vscode.commands.registerCommand(commandId, commandHandler);
+		const subscription = vsc.commands.registerCommand(commandId, commandHandler);
 		logger.debug(`registerCommand:${commandId}`);
 		context.subscriptions.push(subscription);
 	};
 	const registerTextEditorCommand = (commandId: string, commandHandler: (...args: any[]) => any) => {
-		const subscription = vscode.commands.registerCommand(commandId, commandHandler);
+		const subscription = vsc.commands.registerCommand(commandId, commandHandler);
 		logger.debug(`registerTextEditorCommand:${commandId}`);
 		context.subscriptions.push(subscription);
 	};
 	return [registerCommand, registerTextEditorCommand];
 }
 
+export function completionRegisterFactory(context: vsc.ExtensionContext) {
+	const registerCompletionItemProvider = (selector: vsc.DocumentSelector, completionItemProvider: vsc.CompletionItemProvider, triggerCharacters = []) => {
+		const subscription = vsc.languages.registerCompletionItemProvider(selector, completionItemProvider);
+		context.subscriptions.push(subscription);
+	};
+	return registerCompletionItemProvider;
+}
+
 export function execCmd<T = unknown>(cmd: string | { command: string; args: object }): () => Thenable<T> {
 	if (typeof cmd === 'string') {
 		return () => {
 			logger.info(`Execute VSCodeCommand: ${cmd}`);
-			return vscode.commands.executeCommand(cmd);
+			return vsc.commands.executeCommand(cmd);
 		};
 	} else if (cmd.command && typeof cmd.command === 'string') {
 		return () => {
 			logger.info(`Execute VSCodeCommand: ${cmd.command}, args: ${cmd.args}`);
-			return vscode.commands.executeCommand(cmd.command, cmd.args);
+			return vsc.commands.executeCommand(cmd.command, cmd.args);
 		};
 	} else {
 		const msg = `Provide wrong command payload: ${cmd}`;
-		vscode.window.showErrorMessage(msg);
+		vsc.window.showErrorMessage(msg);
 		throw new Error(msg);
 	}
 }
@@ -41,8 +49,12 @@ export function insertSnippet(snippet: string) {
 	});
 }
 
+
+/**
+ * Exec a shell command
+ */
 export function execShell(cmd: string) {
-	return () => vscode.window.withProgress({ location: vscode.ProgressLocation.Notification }, async (progress) => {
+	return () => vsc.window.withProgress({ location: vsc.ProgressLocation.Notification }, async (progress) => {
 		progress.report({
 			message: `Execute shell command: ${cmd}...`,
 		});
