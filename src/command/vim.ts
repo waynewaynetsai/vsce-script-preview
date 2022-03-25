1ae59088246aea53cd200bb340fe75a5cc1bdf6e
waynewaynetsai <wayne040904007@gmail.com>
2022-03-25 22:01:43 +0800

    feat(vim.ts): implement typeKeys utility function, support rest parameter for invokeCommands function

--- src/command/vim.ts
+++ src/command/vim.ts
@@ -10,12 +10,22 @@ export const makeTypeCommand = (input: string): TypeCommand => {
     };
 };
 
-export function invokeCommands(commands: CommandFactory[]) {
+export function invokeCommands(...args: any[]) {
+    let commands: CommandFactory[];
+    if (args.length === 1 && Array.isArray(args[0])) {
+        commands = args[0];    
+    } else {
+        commands = args;
+    } 
 	return commands.reduce((acc, curr) => acc.then(_ => curr()), Promise.resolve(null) as Thenable<null>);
 }
 
 export function type(typeText: string): () => Thenable<void> {
 	return () => vscode.commands.executeCommand("type", { text: typeText });
+}
+
+export function typeKeys(typeTexts: string[]): () => Thenable<void> {
+    return () => typeTexts.reduce((acc, curr) => acc.then(_ => type(curr)()), Promise.resolve(null) as Thenable<void>);
 }
 
 export function typeCommands(texts: string[]): (() => Thenable<void>)[] {
