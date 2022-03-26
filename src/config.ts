7a7646cd4bf06404ef836361ad878c3acfbee26c
waynewaynetsai <wayne040904007@gmail.com>
2022-03-26 23:57:24 +0800

    feat(config.ts): add default commands for dynamics commands

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,18 @@ export enum WorkspaceConfig {
     ProjectPath = 'vsce-script.projectPath',
     CommandPrefix = 'vsce-script.commandPrefix'
 }
+
+export const defaultContributionCommands = [
+    {
+        "title": "Vsce Script: Reload Script",
+        "command": "vsce-script.reloadScript"
+    },
+    {
+        "title": "Vsce Script: Open Script Project",
+        "command": "vsce-script.openProject"
+    },
+    {
+        "title": "Vsce Script: Create Script Project",
+        "command": "vsce-script.createProject"
+    }
+];
