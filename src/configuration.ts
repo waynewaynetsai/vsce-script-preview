0cdcf4037c2433c1ccb6c1acb37426a9d4c7c3d7
waynewaynetsai <wayne040904007@gmail.com>
2022-03-28 23:56:08 +0800

    feat: implment configuration.ts

---
+++ src/configuration.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+import { Global } from './global';
+
+export class Configuration {
+
+    public commandPrefix = 'vsce-script';
+
+    public projectPath = '';
+
+    constructor() { }
+
+    public load() {
+        const configs: { [key: string]: any } = Global.isTesting
+            ? Global.mockConfiguration!
+            : vscode.workspace.getConfiguration('vsce-script');
+
+        for (const option in this) {
+            const val = configs[option];
+            if (val) {
+                this[option] = val;
+            }
+        }
+        return configs;
+    }
+}
