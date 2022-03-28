9565e8b75821ed9c4eb918cab013e7830c7077d0
waynewaynetsai <wayne040904007@gmail.com>
2022-03-28 12:55:04 +0800

    feat(interactive): provide options for dropdown, input and confirm component

--- src/interactive/index.ts
+++ src/interactive/index.ts
@@ -1,20 +1,23 @@
 import * as vscode from 'vscode';
 
-export const dropdown = async (title: string, items: string[], placeHolder: string) => {
+export const dropdown = async (title: string, items: string[], placeHolder: string, options: vscode.QuickPickOptions = {}) => {
    return await vscode.window.showQuickPick(items, {
        canPickMany: false,
        placeHolder,
-       title
+       title,
+       ...options
    });
 };
 
-export const input = async (prompt: string, placeHolder: string) => (await vscode.window.showInputBox({
+export const input = async (prompt: string, placeHolder: string, options: vscode.InputBoxOptions = {}) => (await vscode.window.showInputBox({
     placeHolder,
     prompt,
+    ...options
 })) || '';
 
-export const confirm = async (title: string, placeHolder: 'Yes' | 'No' = 'No') => await vscode.window.showQuickPick(['Yes', 'No'], {
+export const confirm = async (title: string, placeHolder: 'Yes' | 'No' = 'No', options: vscode.QuickPickOptions = {}) => await vscode.window.showQuickPick(['Yes', 'No'], {
     title,
     canPickMany: false,
     placeHolder,
+    ...options
 }).then(answer => answer === 'Yes');
