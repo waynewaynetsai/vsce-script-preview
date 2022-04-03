f7ef2dcdcf4c2e4185057479e7e5b8f07b3c3c38
waynewaynetsai <wayne040904007@gmail.com>
2022-04-03 10:12:00 +0800

    workaround ctors not define error

--- src/instantiator.ts
+++ src/instantiator.ts
@@ -6,45 +6,42 @@ import { ScriptLoader } from './loader';
 import { CommandRegistry } from './registry';
 import { Configuration } from './configuration';
 
-const constructors = [
-    Library,
-    ScriptLoader,
-    CommandRegistry,
-    Configuration
-];
-
 export class Instantiator {
+  private static _container: Container;
 
-    private static _container: Container;
-  
-    public static get container(): Container {
-      return Instantiator._container;
-    }
-  
-    public static set container(value: Container) {
-      Instantiator._container = value;
-    }
-  
-    public static async startup(context: vscode.ExtensionContext) {
-      const container = await this.setupIocContainer(context);
-      Instantiator.container = container;
-      await this.instantiate(container);
-      return Instantiator.container;
-    }
-  
-    private static async setupIocContainer(context?: vscode.ExtensionContext) {
-      const container = new Container();
-      container.registerObject(Instance.ExtensionContext, context);
-      for (const Constructor of constructors) {
-        container.bind(Constructor);
-      }
-      return container;
-    }
-  
-    private static async instantiate(container: Container) {
-      for (const Constructor of constructors) {
-        await container.getAsync(Constructor);
-      }
-    }
-  
+  public static get container(): Container {
+    return Instantiator._container;
   }
+
+  public static set container(value: Container) {
+    Instantiator._container = value;
+  }
+
+  public static async startup(context: vscode.ExtensionContext) {
+    const ctors = [
+      ScriptLoader,
+      Library,
+      CommandRegistry,
+    ];
+    const container = await this.setupIocContainer(context, ctors);
+    Instantiator.container = container;
+    await this.instantiate(container, ctors);
+    return Instantiator.container;
+  }
+
+  private static async setupIocContainer(context: vscode.ExtensionContext, ctors: any[]) {
+    const container = new Container();
+    container.registerObject(Instance.ExtensionContext, context);
+    for (const Constructor of ctors) {
+      container.bind(Constructor);
+    }
+    return container;
+  }
+
+  private static async instantiate(container: Container, ctors) {
+    for (const Constructor of ctors) {
+      await container.getAsync(Constructor);
+    }
+  }
+
+}
