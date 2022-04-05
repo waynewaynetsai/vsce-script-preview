118d806628aab245cff9461c2996706adbc19de7
waynewaynetsai <wayne040904007@gmail.com>
2022-04-05 23:58:47 +0800

    feat(registry.ts): implement isTsProject utils function

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,2 +1,3 @@
 export * from './registry';
 export * from './handler';
+export * from './utils';
---
+++ src/registry/utils.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function isTsProject() {
+    const existProjectPath = vscode.workspace.getConfiguration('vsce-script').get<string>('projectPath');
+    console.log('existProjectPath', existProjectPath);
+    if (!existProjectPath || existProjectPath === '') return false;
+    const tsConfigPath = path.join(existProjectPath, 'tsconfig.json');
+    return fs.existsSync(tsConfigPath);
+}
