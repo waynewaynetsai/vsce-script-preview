3a72d95709d1d10254775a53f9381b163b5017e7
waynewaynetsai <wayne040904007@gmail.com>
2022-04-06 13:16:14 +0800

    feat(editor/index.ts): move getCursorPosition function to utils.ts

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -4,28 +4,6 @@ export * from './utils';
 
 import { typeCommands, invokeCommands } from "../command";
 
-export function getCurrentLine(editor: vsc.TextEditor): string {
-	return editor.document.lineAt(editor.selection.active.line).text;
-}
-
-export function getCursorPosition(): vsc.Position | undefined {
-	const editor = vsc.window.activeTextEditor;
-	if (!editor) return;
-	return editor.selection.active;
-}
-
-export function setCursorPosition(pos: vsc.Position): () => Promise<any> {
-	return () => new Promise((resolve, _reject) => {
-		const editor = vsc.window.activeTextEditor;
-		if (!editor) return;
-		setTimeout(() => {
-			const endPos = new vsc.Position(pos.line, pos.character - 1);
-			const selection = new vsc.Selection(endPos, endPos);
-			editor.selection = selection;
-			resolve(null);
-		}, 0);
-	});
-}
 
 export function switchToInsertModeSelection(): Promise<void> {
 	return new Promise((resolve, _reject) => {
@@ -43,6 +21,19 @@ export function switchToInsertModeSelection(): Promise<void> {
 				sel.end.character
 			));
 			resolve();
+		}, 0);
+	});
+}
+
+export function setCursorPosition(pos: vsc.Position): () => Promise<any> {
+	return () => new Promise((resolve, _reject) => {
+		const editor = vsc.window.activeTextEditor;
+		if (!editor) return;
+		setTimeout(() => {
+			const endPos = new vsc.Position(pos.line, pos.character - 1);
+			const selection = new vsc.Selection(endPos, endPos);
+			editor.selection = selection;
+			resolve(null);
 		}, 0);
 	});
 }
