0ad1bc097879ae84c47799a138b1d28821171ec6
waynewaynetsai <wayne040904007@gmail.com>
2022-04-06 13:12:12 +0800

    feat(library.ts): add interactive functions, spawnShell commands,typeKeys function

--- src/library.ts
+++ src/library.ts
@@ -1,9 +1,12 @@
+import { SpawnOptions } from 'child_process';
 import { inject, provide } from 'injection';
 import * as vscode from 'vscode';
 import * as packageJSON from '../package.json';
-import { commandRegisterFactory, completionRegisterFactory, execCmd, execShell, invokeCommands, runMacro, type } from './command';
+import { completionRegisterFactory, execCmd, execShell, invokeCommands, runMacro, spawnShell, type, typeKeys } from './command';
 import { createNewFile, createNewFolder, getCharAt, getCharUnderCursor, getCursorPosition, getFirstCharOnLine, getSelectedText, setCursorPosition, switchToInsertModeSelection } from './editor';
 import { Instance } from './instance';
+import { confirm, dropdown, input } from './interactive';
+import { commandQuickpick } from './registry';
 import { CommandRegistry } from './registry/registry';
 
 @provide()
@@ -23,13 +26,20 @@ export class Library {
             vim: {
                 switchToInsertModeSelection,
                 runMacro,
-                type
+                type,
+                typeKeys
             },
             command: {
                 registerCommand: (commandId: string, handler: (...args) => any) => this.registry.registerCommand(commandId, handler),
                 invokeCommands,
                 execShell,
-                execCmd
+                spawnShell,
+                execCmd,
+            },
+            promise: {
+                execShell: async (cmd: string) => execShell(cmd),
+                spawnShell: async (...args: [cmd: string, args?: string[] | undefined, option?: SpawnOptions | undefined]) => spawnShell.apply(null, args),
+                execCmd: async (payload: string | { command: string; args: object; }) => execCmd(payload) 
             },
             editor: {
                 registerCompletionProvider,
@@ -41,6 +51,12 @@ export class Library {
                 createNewFolder,
                 getCursorPosition,
                 setCursorPosition
+            },
+            interactive: {
+               confirm, 
+               input,
+               dropdown,
+               commandQuickpick
             }
         };
         return lib;
