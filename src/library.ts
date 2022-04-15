aad8a9b8d1343a60741240f6e4d00500b00217a5
waynewaynetsai <wayne040904007@gmail.com>
2022-04-15 19:25:00 +0800

    refactor(library.ts): adjust library api namespace

--- src/library.ts
+++ src/library.ts
@@ -2,7 +2,7 @@ import { SpawnOptions } from 'child_process';
 import { inject, provide } from 'injection';
 import * as vscode from 'vscode';
 import * as packageJSON from '../package.json';
-import { completionRegisterFactory, execCmd, execShell, invokeCommands, runMacro, spawnShell, type, typeCharUnderCursor, typeKeys } from './command';
+import { completionRegisterFactory, execCmd, execShell, invokeCommands, runAutomation, runCommands, runMacro, spawnShell, type, typeCharUnderCursor, typeKeys } from './command';
 import { createNewFile, createNewFolder, findFirstOccurCharAtDocument, findFirstOccurCharAtLine, getCharAt, getCharUnderCursor, getCurrentLine, getCursorPosition, getFirstCharOnLine, getLine, getSelectedText, setCursorPosition, switchToInsertModeSelection } from './editor'; import { Instance } from './instance';
 import { confirm, dropdown, input } from './interactive';
 import { logger } from './logger';
@@ -20,33 +20,29 @@ export class Library {
 
     public version: string = packageJSON.version;
 
-    getLatestLib(context: vscode.ExtensionContext) {
+    getLatestLib(context: vscode.ExtensionContext)  {
         const registerCompletionProvider = completionRegisterFactory(context);
         const lib = {
             version: this.version,
             automation: {
-                operation: {
-                    typeCharUnderCursor,
-                    type,
-                    typeKeys,
-                    execShell,
-                    spawnShell,
-                    execCmd,
-                    setCursorPosition,
-                    switchToInsertModeSelection,
-                },
-                runner: {
-                    invokeCommands,
-                    runMacro,
-                }
+                typeCharUnderCursor,
+                type,
+                typeKeys,
+                execCmd,
+                execShell,
+                spawnShell,
+                runMacro,
+                runAutomation,
+                runCommands,
             },
             commands: {
                 registerCommand: (commandId: string, handler: (...args: any) => any) => this.registry.registerCommand(commandId, handler),
+                invokeCommands
             },
             promise: {
+                execCmd: (payload: string | { command: string; args: object; }) => execCmd(payload),
                 execShell: (cmd: string) => execShell(cmd),
                 spawnShell: (...args: [cmd: string, args?: string[] | undefined, option?: SpawnOptions | undefined]) => spawnShell.apply(null, args),
-                execCmd: (payload: string | { command: string; args: object; }) => execCmd(payload),
                 typeCharUnderCursor: () => typeCharUnderCursor(),
                 type: (text: string) => type(text)(),
                 typeKeys: (texts: string[]) => typeKeys(texts),
