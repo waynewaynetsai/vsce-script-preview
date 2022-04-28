6a0ecef1e0a3452c6dc9d184b46bafe65395efaf
waynewaynetsai <wayne040904007@gmail.com>
2022-04-28 13:52:21 +0800

    fix(editor.ts): fix switchToInsertModeSelection problem with promise

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -4,25 +4,26 @@ export * from './utils';
 
 import { runMacro } from "../command";
 
+async function switchToInsertModeSelectionMacroTask() {
+	const editor = vsc.window.activeTextEditor;
+	const prevSel = editor?.selections;
+	if (!prevSel) return;
+	const enterInsertMode = ["<Esc>", "i"];
+	await runMacro(enterInsertMode);
+	setTimeout(() => {
+		editor.selection = new vsc.Selection(0, 0, 0, 0);
+		editor.selections = prevSel.map((sel: vsc.Selection) => new vsc.Selection(
+			sel.start.line,
+			sel.start.character,
+			sel.end.line,
+			sel.end.character
+		));
+	}, 0);
+}
 
 export async function switchToInsertModeSelection(): Promise<void> {
-	return new Promise(async (resolve, _reject) => {
-		const editor = vsc.window.activeTextEditor;
-		const prevSel = editor?.selections;
-		if (!prevSel) return;
-		const enterInsertMode = ["<Esc>", "i"];
-		await runMacro(enterInsertMode);
-		setTimeout(() => {
-			editor.selection = new vsc.Selection(0, 0, 0, 0);
-			editor.selections = prevSel.map((sel: vsc.Selection) => new vsc.Selection(
-				sel.start.line,
-				sel.start.character,
-				sel.end.line,
-				sel.end.character
-			));
-			resolve();
-		}, 0);
-	});
+	switchToInsertModeSelectionMacroTask();
+	return Promise.resolve();
 }
 
 export function setCursorPosition(pos: vsc.Position): Promise<any> {
